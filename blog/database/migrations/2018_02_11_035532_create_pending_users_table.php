40d2807548f03ec991a876e4270848be171f3f2a
bryan <bmsandoval@gmail.com>
2018-02-10 22:01:00 -0700

    few minor changes

--- blog/database/migrations/2018_02_11_035532_create_pending_users_table.php
+++ blog/database/migrations/2018_02_11_035532_create_pending_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PendingUsers extends Migration
+class PendingUsersTable extends Migration
 {
     /**
      * Run the migrations.

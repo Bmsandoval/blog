549a18e74da23ec56da7e2e704db80d2e1542b7f
bryan <bmsandoval@gmail.com>
2018-04-16 21:24:07 -0600

    fixed buttons and alignment

--- blog/resources/views/posts/list.blade.php
+++ blog/resources/views/posts/list.blade.php
@@ -4,25 +4,32 @@
 
         <section class="jumbotron text-center">
             <div class="container">
-                <h1 class="jumbotron-heading">Blog Posts</h1>
-                <p class="lead text-muted">Get a little taste of the life of my friends and I.
-                    Come on a journey and learn a little along the way.</p>
-                @if(Auth::check())
-                    <div class="row">
-                        <div class="col-sm-3"></div>
-                        <div class="col-sm-2">
-                            <a href="/create" class="ui-button ui-corner-all ui-widget">Create Post</a>
-                        </div>
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-2">
-                            @if($stash)
-                                <a href="/posts" class="ui-button ui-corner-all ui-widget">View Posts</a>
-                            @elseif(!$stash)
-                                <a href="/posts/stash" class="ui-button ui-corner-all ui-widget">View Stash</a>
-                            @endif
-                        </div>
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-4">
+                        <h1 class="jumbotron-heading">Blog Posts</h1>
                     </div>
-                @endif
+                    <div class="col-lg-8 offset-lg-3">
+                        <p class="lead text-muted">Get a little taste of the life of my friends and I. Come on a journey and learn a little along the way.</p>
+                    </div>
+                </div>
+                <p>
+                    @auth
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-2">
+                                <a href="/create" class="btn btn-outline-primary">Create Post</a>
+                            </div>
+                            <div class="col-sm-5"></div>
+                            <div class="col-sm-2">
+                                @if($stash)
+                                    <a href="/posts" class="btn btn-outline-success">View Posts</a>
+                                @elseif(!$stash)
+                                    <a href="/posts/stash" class="btn btn-outline-warning">View Stash</a>
+                                @endif
+                            </div>
+                        </div>
+                    @endauth
+                </p>
             </div>
         </section>
 
